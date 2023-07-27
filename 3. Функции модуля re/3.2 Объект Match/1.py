7b3233a82192a695f318360b1db4a4fcd9a43d07
RAX3R <45417916+vqpv@users.noreply.github.com>
2023-07-27 23:52:33 +0300

    3.2 - 1

---
+++ 3. Функции модуля re/3.2 Объект Match/1.py
@@ -0,0 +1,11 @@
+import re
+
+match = re.match(input(), input())
+
+print(match.group())
+print(match.start())
+print(match.end())
+print(match.pos)
+print(match.endpos)
+print(match.re)
+print(match.string)
