87792b37f519d90d006d686349ee4db9e66c5311
RAX3R <45417916+vqpv@users.noreply.github.com>
2023-08-22 10:36:38 +0300

    3.11 - 1

---
+++ 3. Функции модуля re/3.11 re.escape()/1.py
@@ -0,0 +1,3 @@
+import re
+
+print(re.escape(r'<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ !"#$%&\'()*+,-./0123456789:;'))
